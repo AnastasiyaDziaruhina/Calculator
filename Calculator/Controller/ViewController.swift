2c26bdf385ba850be49f47534b72dfc7e299e1bc
stacysfish <st.vitalievna@yandex.ru>
2022-07-20 13:29:37 +0200

    Initial commit

---
+++ Calculator/Controller/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Stacy on 24.06.22.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a double")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    private var calculator = CalculatorLogic()
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        isFinishedTypingNumber = true
+        
+        calculator.setNumber(displayValue)
+        
+        if let calcMethdod = sender.currentTitle {
+            
+            if let result = calculator.calculate(symbol: calcMethdod) {
+                displayValue = result
+            }
+            
+        }
+        
+    }
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                
+                if let labelText = displayLabel.text {
+                    if !labelText.contains(".") || numValue != "." {
+                        displayLabel.text = labelText + numValue
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Calculator</h1>
+
+  A calculator app. 
+
+
